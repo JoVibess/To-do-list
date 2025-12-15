3a564e3ad767743689125a1fa4811935f6bb2731
JoVibess <joeydecroix.pro@gmail.com>
2025-12-15 10:35:01 +0100

    UPD [auth] debug route

---
+++ migrations/20251215092450-add-role-to-user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('user', 'role', {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+      defaultValue: 'client'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('user', 'role');
+  }
+};
